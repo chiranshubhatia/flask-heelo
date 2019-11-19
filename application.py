88e14df54769d89448891395ab58f6f9c5a1882c
chiranshu bhatia <50030365+chiranshubhatia@users.noreply.github.com>
2019-11-19 16:07:34 +0530

    Update application.py

--- application.py
+++ application.py
@@ -6,5 +6,14 @@ app=Flask(__name__)
 @app.route('/')
 def extract_text():
   print(" hello world")
-  return "welcome to the page"
-app.run()
+  return '''
+<html><head><title>Upload new File</title>
+    </head><body><h1>OCR extraction for HDCF form 1500</h1>
+     <h2>HIT button for demo run</h2>
+  
+    <form method="post" enctype="multipart/form-data">
+          
+         <input type="submit" value="RUN">
+         </p>
+         </form></h2><div style="background-color: rgb(255, 143, 0); display: none; color: white; text-align: center; position: fixed; top: 0px; left: 0px; width: 100%; height: auto; min-width: 100%; min-height: auto; max-width: 100%; font: 12px &quot;Helvetica Neue&quot;, Helvetica, Arial, Geneva, sans-serif; cursor: pointer; padding: 5px;"><span style="color: white; font: 12px &quot;Helvetica Neue&quot;, Helvetica, Arial, Geneva, sans-serif;">You have turned off the paragraph player. You can turn it on again from the options page.</span><img src="chrome-extension://gfjopfpjmkcfgjpogepmdjmcnihfpokn/img/icons/icon-close_16.png" style="width: 20px; height: auto; min-width: 20px; min-height: auto; max-width: 20px; float: right; margin-right: 10px;"></div></body></html>
+'''
